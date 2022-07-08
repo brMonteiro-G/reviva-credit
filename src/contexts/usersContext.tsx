cf9b8137395b9d2b503c19a57a04f10e35d1797f
Danielscript1 <danielsousagit@gmail.com>
2022-07-08 17:58:15 -0300

    refactor(Context):users change name Interface

--- src/contexts/usersContext.tsx
+++ src/contexts/usersContext.tsx
@@ -1,6 +1,9 @@
-import { ProviderProps } from "@/types/IUProviderProps";
 import { IUser } from "@/types/IUser";
-import { createContext, useState } from "react";
+import { createContext, ReactNode, useState } from "react";
+
+export interface UsersProviderProps {
+  children: ReactNode;
+}
 
 interface IUserContextProps {
   users: IUser[];
@@ -13,7 +16,7 @@ export const UsersContext = createContext<IUserContextProps>({
 });
 UsersContext.displayName = "Users";
 
-export const UsersProvider = ({ children }: ProviderProps) => {
+export const UsersProvider = ({ children }: UsersProviderProps) => {
   const [users, setUsers] = useState<IUser[]>([]);
   return (
     <UsersContext.Provider value={{ users, setUsers }}>
