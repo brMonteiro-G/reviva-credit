7bc6488619da00e9c24d7e819049727249f8e058
Lucas Silva <lucassilva.uern@gmail.com>
2022-07-08 14:55:44 -0300

    refactor(styles): refactor style cardinfo

--- src/components/CardInfo/styles.ts
+++ src/components/CardInfo/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const SectionCard = styled.section`
-  max-width: 375px;
+  max-width: 325px;
   max-height: 200px;
   background-color: #eeeeee;
   padding: 2rem 0rem;
