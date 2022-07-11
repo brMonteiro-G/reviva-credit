79ea2b8fd1345a8bd75310cd4fe3fa2ed1075613
Lucas Silva <lucassilva.uern@gmail.com>
2022-07-11 15:58:09 -0300

    fix(router):changing routes to use contexts

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
 import { CardsProvider } from "./contexts/cardsContext";
 import { TransactionsProvider } from "./contexts/transactionsContext";
 import { UsersProvider } from "./contexts/usersContext";
-import PageDefault from "./templates";
+import Details from "./pages/details";
+import Home from "./pages/home";
+import Main from "./templates/Main";
 
 export default function AppRouter() {
   return (
@@ -11,18 +12,18 @@ export default function AppRouter() {
       <UsersProvider>
         <CardsProvider>
           <TransactionsProvider>
-            <Routes>
-              <Route path="/" element={<PageDefault />}>
-                <Route path="/wallet" element={""} />
-                <Route path="/details" element={""} />
-                <Route path="/settings" element={""} />
-              </Route>
-              <Route path="*" element={""} />
-            </Routes>
+            <Main>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/wallet" element={"Wallet"} />
+                <Route path="/settings" element={"Settings"} />
+                <Route path="/details" element={<Details />} />
+                <Route path="*" element={"404"} />
+              </Routes>
+            </Main>
           </TransactionsProvider>
         </CardsProvider>
       </UsersProvider>
-      <Footer />
     </Router>
   );
 }
