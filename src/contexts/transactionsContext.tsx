75d80e0ed74aa6aed6c385f18d493efb36a5130c
Lucas Silva <lucassilva.uern@gmail.com>
2022-07-11 21:46:58 -0300

    fix(types):correcting transaction type name

--- src/contexts/transactionsContext.tsx
+++ src/contexts/transactionsContext.tsx
@@ -1,4 +1,5 @@
-import { IUTransactions } from "@/types/IUTransactions";
+
+import { ITransaction } from "@/types/ITransaction";
 import { createContext, ReactNode, useState } from "react";
 
 interface TransactionsProviderProps {
@@ -6,20 +7,20 @@ interface TransactionsProviderProps {
 }
 
 interface TransactionsContextProps {
-  transactions: IUTransactions[];
-  setTransactions: (transactions: IUTransactions[]) => void;
+  transactions: ITransaction[];
+  setTransactions: (transactions: ITransaction[]) => void;
 }
 
 export const TransactionsContext = createContext<TransactionsContextProps>({
   transactions: [],
-  setTransactions: (transactions: IUTransactions[]) => [],
+  setTransactions: (transactions: ITransaction[]) => [],
 });
 TransactionsContext.displayName = "transactions";
 
 export const TransactionsProvider = ({
   children,
 }: TransactionsProviderProps) => {
-  const [transactions, setTransactions] = useState<IUTransactions[]>([]);
+  const [transactions, setTransactions] = useState<ITransaction[]>([]);
   return (
     <TransactionsContext.Provider value={{ transactions, setTransactions }}>
       {children}
--- src/types/IUTransactions.ts
+++
@@ -1,7 +0,0 @@
-export interface IUTransactions {
-  id: string;
-  cardId: string;
-  value: number;
-  description: string;
-  date: string | Date;
-}
